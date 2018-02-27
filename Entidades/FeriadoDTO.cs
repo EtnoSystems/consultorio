7eafec24ad6cb9563fece8f0f4d3f7fda081ce8f
matiasff <matybienalto@gmail.com>
2018-02-27 20:05:38 -0300

    Se agregaron varias clases, a revisar

---
+++ Entidades/FeriadoDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAO;
+
+namespace Entidades
+{
+    public class FeriadoDTO
+    {
+        public int Id { get; set; }
+        public string Fecha{ get; set; }
+        public string Descripcion{ get; set; }
+    }
+}
