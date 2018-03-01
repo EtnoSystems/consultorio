cd4e2604c399eb3064827f8e2e20a9558e711f1b
JJ <javier_robas@hotmail.com>
2018-03-01 02:34:12 -0300

    se agregaron entradas a la tabla feriados

--- inserts feriados.sql
+++ inserts feriados.sql
@@ -1,3 +1,18 @@
 -- INSERTS DE LA TABLA FERIADOS
 
-insert into dbo.feriado (fecha,descripcion) values ('','');
+insert into dbo.feriado (fecha,descripcion) values ('2018-03-24','Día nacional por la memoria, la verdad y la justicia');
+insert into dbo.feriado (fecha,descripcion) values ('2018-03-29','Jueves santo');
+insert into dbo.feriado (fecha,descripcion) values ('2018-03-30','Viernes santo');
+insert into dbo.feriado (fecha,descripcion) values ('2018-04-02','Día del veterano y de los caídos en la guerra de Malvinas');
+insert into dbo.feriado (fecha,descripcion) values ('2018-04-30','Feriado puente turístico');
+insert into dbo.feriado (fecha,descripcion) values ('2018-05-01','Día del trabajador');
+insert into dbo.feriado (fecha,descripcion) values ('2018-05-25','Día de la revolución de mayo');
+insert into dbo.feriado (fecha,descripcion) values ('2018-06-20','Paso a la inmortalidad del Gral Manuel Belgrano');
+insert into dbo.feriado (fecha,descripcion) values ('2018-07-03','Día de la independencia');
+insert into dbo.feriado (fecha,descripcion) values ('2018-08-20','Paso a la inmortalidad del Gral José de San Martín');
+insert into dbo.feriado (fecha,descripcion) values ('2018-10-15','Dia del respeto a la diversidad cultural');
+insert into dbo.feriado (fecha,descripcion) values ('2018-11-19','Día de la soberanía nacional');
+insert into dbo.feriado (fecha,descripcion) values ('2018-12-08','Inmaculada concepción de María');
+insert into dbo.feriado (fecha,descripcion) values ('2018-12-24','Feriado puente turístico');
+insert into dbo.feriado (fecha,descripcion) values ('2018-12-25','Navidad');
+insert into dbo.feriado (fecha,descripcion) values ('2018-12-31','Feriado puente turísitco');
--- inserts generales.sql
+++ inserts generales.sql
@@ -323,6 +323,24 @@ insert into dbo.especialidad (denominacion) values ('Traumatología y ortopedia'
 insert into dbo.especialidad (denominacion) values ('Traumatología y ortopedia infantil');
 insert into dbo.especialidad (denominacion) values ('Urología');
 
+-- INSERTS DE LA TABLA FERIADOS
+insert into dbo.feriado (fecha,descripcion) values ('2018-03-24','Día nacional por la memoria, la verdad y la justicia');
+insert into dbo.feriado (fecha,descripcion) values ('2018-03-29','Jueves santo');
+insert into dbo.feriado (fecha,descripcion) values ('2018-03-30','Viernes santo');
+insert into dbo.feriado (fecha,descripcion) values ('2018-04-02','Día del veterano y de los caídos en la guerra de Malvinas');
+insert into dbo.feriado (fecha,descripcion) values ('2018-04-30','Feriado puente turístico');
+insert into dbo.feriado (fecha,descripcion) values ('2018-05-01','Día del trabajador');
+insert into dbo.feriado (fecha,descripcion) values ('2018-05-25','Día de la revolución de mayo');
+insert into dbo.feriado (fecha,descripcion) values ('2018-06-20','Paso a la inmortalidad del Gral Manuel Belgrano');
+insert into dbo.feriado (fecha,descripcion) values ('2018-07-03','Día de la independencia');
+insert into dbo.feriado (fecha,descripcion) values ('2018-08-20','Paso a la inmortalidad del Gral José de San Martín');
+insert into dbo.feriado (fecha,descripcion) values ('2018-10-15','Dia del respeto a la diversidad cultural');
+insert into dbo.feriado (fecha,descripcion) values ('2018-11-19','Día de la soberanía nacional');
+insert into dbo.feriado (fecha,descripcion) values ('2018-12-08','Inmaculada concepción de María');
+insert into dbo.feriado (fecha,descripcion) values ('2018-12-24','Feriado puente turístico');
+insert into dbo.feriado (fecha,descripcion) values ('2018-12-25','Navidad');
+insert into dbo.feriado (fecha,descripcion) values ('2018-12-31','Feriado puente turísitco');
+
 --INSERTS PARA LAS OBRAS SOCIALES
 insert into dbo.obra_social (denominacion,nombre_largo,plus,activa) values ('AMUR','Asociación Mutual Ruralista',250,'TRUE');
 insert into dbo.obra_social (denominacion,nombre_largo,plus,activa) values ('DASUTEN','Dirección de Acción Social de la UTN',300,'TRUE');
