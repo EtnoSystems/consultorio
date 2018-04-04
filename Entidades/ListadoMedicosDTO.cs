fe945c0a795635d5fd3a22d56ad037e9b114899a
JJ <javier_robas@hotmail.com>
2018-04-04 01:55:55 -0300

    Se comenzo a trabajar con los filtros en los listados medicos del formulario de carga de un servicio

---
+++ Entidades/ListadoMedicosDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class ListadoMedicosDTO
+    {
+        public int Id { get; set; }
+        public string Apellido_y_Nombre { get; set; }
+    }
+}
---
+++ Logica/GestorServicio.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAO;
+using Entidades;
+
+namespace Logica
+{
+    public class GestorServicio
+    {
+        public List<ListadoMedicosDTO> Filtro_Por_ObraSocial (Nullable<int> id_OS) //no contempla el caso de que id_OS sea null
+        {
+            List<ListadoMedicosDTO> listaResultados = new List<ListadoMedicosDTO>();
+            
+            try
+            {
+                using (consultoriosEntities dbContext = new consultoriosEntities())
+                {
+
+                    var OS = (from n in dbContext.Obra_social
+                                 where n.Id == id_OS
+                                 select n).FirstOrDefault();
+
+                    
+                    foreach (Persona persona in OS.Persona)
+                    {
+                        if (persona.Matricula != null)
+                        {
+                            ListadoMedicosDTO medico = new ListadoMedicosDTO();
+                            medico.Id = persona.Id;
+                            medico.Apellido_y_Nombre = persona.Apellido + ", " + persona.Nombre;
+                            listaResultados.Add(medico);
+                        }
+                    }
+
+                    return listaResultados;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
